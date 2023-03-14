9c11894a3c735005f3b834af2751cfb9885cf6c2
vqcuong <35632030+vqcuong@users.noreply.github.com>
2023-03-14 23:51:20 +0700

    Update AppConfLoader

--- src/main/scala/com/sparkfast/spark/app/config/AppConfLoader.scala
+++ src/main/scala/com/sparkfast/spark/app/config/AppConfLoader.scala
@@ -1,14 +1,22 @@
 package com.sparkfast.spark.app.config
 
 import com.sparkfast.core.jackson.YamlIO
+import com.sparkfast.core.logger.LoggerMixin
 
 import java.io.FileInputStream
 
-object AppConfLoader {
-  def loadFromResource[T](resourcePath: String)(implicit mf: Manifest[T]): AppConf =
-    YamlIO.fromInputStream(mf.runtimeClass.getClassLoader.getResourceAsStream(resourcePath), classOf[AppConf])
+object AppConfLoader extends LoggerMixin {
+  def loadFromResource[T](resourcePath: String)(implicit mf: Manifest[T]): AppConf = {
+    val inputStream = mf.runtimeClass.getClassLoader.getResourceAsStream(resourcePath)
+    val str = scala.io.Source.fromInputStream(inputStream).mkString
+    log.info(s"Application config:\n$str")
+    YamlIO.fromString(str, classOf[AppConf])
+  }
 
   def loadFromFile(filePath: String): AppConf = {
-    YamlIO.fromInputStream(new FileInputStream(filePath), classOf[AppConf])
+    val inputStream = new FileInputStream(filePath)
+    val str = scala.io.Source.fromInputStream(inputStream).mkString
+    log.info(s"Application config:\n$str")
+    YamlIO.fromString(str, classOf[AppConf])
   }
 }
